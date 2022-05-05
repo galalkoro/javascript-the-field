4a8b87721ae03e4cf6e37f42f8a160a5eef94a50
galalkoro <koro-galal@outlook.com>
2022-05-05 10:43:09 +0200

    solve base-alert &prompt

--- js-complete-course/01-base/02-var-prompt/script.js
+++ js-complete-course/01-base/02-var-prompt/script.js
@@ -12,5 +12,11 @@
 (function() {
 
     // your code here
+    let person = prompt("enter name, Galal");
+    if (person != null){
+        document.getElementById("explain").innerHTML =
+        "Hello " + person + "! How are you today?";
+    }
+
 
 })();
