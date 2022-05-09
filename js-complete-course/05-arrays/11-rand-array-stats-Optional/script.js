6d598e15887fa25247f86f318e2b912afcc86cb4
galalkoro <koro-galal@outlook.com>
2022-05-09 23:20:20 +0200

    exercise not done completly but chapter finished

--- js-complete-course/05-arrays/11-rand-array-stats-Optional/script.js
+++ js-complete-course/05-arrays/11-rand-array-stats-Optional/script.js
@@ -13,4 +13,46 @@
 
     // your code here
 
+    document.getElementById("run").addEventListener("click", () => {
+        var i;
+        const td = new Array();
+        td[0] = '0';
+        td[1] = '1';
+        td[2] = '2';
+        td[3] = '3';
+        td[4] = '4';
+        td[5] = '7';
+        td[6] = '8';
+        td[7] = '9';
+        td[8] = '10';
+        
+        
+        console.log(td);
+        
+        const numberConverter = td.map(Number);
+        
+        const sum = numberConverter.reduce((accumulator, currentValue) => {
+          return accumulator + currentValue;
+        }, 0);
+        
+        const average = numberConverter.reduce(
+          (accumulator, currentvalue, index, numArray) => {
+            return accumulator + currentvalue / numArray.length;
+          },
+          0
+        );
+        
+        const average2 =
+          numberConverter.reduce(
+            (accumulator, currentValue) => accumulator + currentValue,
+            0
+          ) / numberConverter.length;
+        
+        for (i = 99; i < 100; i++) {
+          console.log(
+            `The sum of all the elements is: ${sum}. <br> The average of all the elements is: ${average2}`
+          );}
+
+    });
+
 })();
