7ce1b831929d1c1441b3bf64ed531741688f1f75
galalkoro <koro-galal@outlok.com>
2022-05-13 14:00:40 +0200

    exercise details done it really hard for me to understand the fetch concept and to strucsize

--- 07-fetch/02-details/script.js
+++ 07-fetch/02-details/script.js
@@ -11,4 +11,36 @@
 
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click", fetchData)
+       
+   async function fetchData() {
+       const res = await fetch("/_shared/api.json");
+       const heroData = await res.json();
+    //console.log(heroData);
+
+    var inPut = document.getElementById("hero-id").value; 
+   
+   // console.log(inPut);
+   heroData.heroes.forEach(element => {
+
+
+    if (element.id == inPut) { 
+       // console.log(element.name);
+       var list = document.createElement('li');
+       var header = document.createElement('h4')
+       var paragrapgh  = document.createElement('p');
+       var  emphasis = document.createElement('em')
+       target.append(list);
+       target.append(paragrapgh);
+       list.append(header);
+       list.append(emphasis);
+       header.innerHTML = element.name;
+       paragrapgh.innerHTML = element.abilities;
+       emphasis.innerHTML = element.alterEgo;
+    } else{
+       console.log(heroData); // on console log 
+    }
+       
+   });
+    }
 })();
